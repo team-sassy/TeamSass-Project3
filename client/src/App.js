166d904d24aa622a5885e8e50eac77325aaeb952
kathleen.e.burns@gmail.com <kathleen.e.burns@gmail.com>
2019-02-09 10:52:11 -0600

    CPR from activity 20-3

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,41 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import SearchForm from "./components/Search";
 import axios from 'axios'
 
 
-class App extends Component {
-  state = {
-    "testValue": "SASSY......",
-  }
-  componentDidMount() {
-    console.log("Mounting App")
-    axios.get("/api/test")
-      .then(result => {
-        console.log(result.data.test)
-        this.setState({"testValue": result.data.test})
-      })
-  }
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>SASS YOUR WAY TO THE TOP</h2>
-        </div>
-        <p className="App-intro">
-          The test value is: {this.state.testValue}
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return <SearchForm />;
 }
 
 export default App;
+
+// class App extends Component {
+//   state = {
+//     "testValue": "SASSY......",
+//   }
+//   componentDidMount() {
+//     console.log("Mounting App")
+//     axios.get("/api/test")
+//       .then(result => {
+//         console.log(result.data.test)
+//         this.setState({"testValue": result.data.test})
+//       })
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h2>SASS YOUR WAY TO THE TOP</h2>
+//         </div>
+//         <p className="App-intro">
+//           The test value is: {this.state.testValue}
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
