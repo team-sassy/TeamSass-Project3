d13145e7cd3dbb006efd1c138f7dea64f09f9655
Lucent <lquint85@gmail.com>
2019-02-11 13:03:00 -0600

    added element to section1 component

--- client/src/App.js
+++ client/src/App.js
@@ -30,6 +30,15 @@ class App extends Component {
         <div id="section1"className="section1 bg-sm">
           <h1 id="section1-h1"> Hello World </h1>
           <p id ="section1-p">this is just a test of what we can do to style the page </p>
+          <br></br>
+          <p id="section1-p2">
+                What is Lorem Ipsum?
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                  when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                   It has survived not only five centuries, but also the leap into electronic typesetting, 
+                   remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+                   Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum </p>
         </div>
         <div id="section2"className="section1 bg-sm">
           <h1 id="section1-h1"> Hello World </h1>
