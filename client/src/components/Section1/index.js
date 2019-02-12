0f65791442f48c63b835b9531e86040a3d480b65
Lucent <lquint85@gmail.com>
2019-02-11 20:12:54 -0600

    scrub components folder

--- client/src/components/Section1/index.js
+++ client/src/components/Section1/index.js
@@ -8,18 +8,14 @@ import "./style.css";
 function Section1() {
   return (
   <>
-        <div id="section1"className="section1 bg-sm">
-          <h1 id="section1-h1"> Hello World </h1>
-          <p id ="section1-p">this is just a test of what we can do to style the page </p>
-          <br></br>
-          <p id="section1-p2">
-                What is Lorem Ipsum?
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                  when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                   It has survived not only five centuries, but also the leap into electronic typesetting, 
-                   remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
-                   Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum </p>
+        <div id="section1" className="section4">
+             <h1 id="section1-h1"> Hello World </h1>
+                 <a id="section1-button" href="flight" class="waves-effect waves-light btn">button</a>
+                    <br></br>
+            <button id="section1-button2" class="btn waves-effect waves-dark" type="submit" name="action">Submit
+            <i class="material-icons right">send</i>
+            </button>
+            
         </div>
 
 </>
