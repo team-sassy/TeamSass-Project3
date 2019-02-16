58538df3567e48623a66654102b491e8e3a31334
Lucent <lquint85@gmail.com>
2019-02-16 09:39:39 -0600

    added modal component, jquery to hmtl, added react dependcies

---
+++ client/src/components/login/index.js
@@ -0,0 +1,17 @@
+// import React from "react";
+
+
+
+
+
+
+// function LoginIn () {
+//     return 
+//          <Button floating fab='horizontal' icon='mode_edit' className='red' large style={{bottom: '45px', right: '24px'}}>
+//             <Button floating icon='insert_chart' className='red'/>
+//             <Button floating icon='format_quote' className='yellow darken-1'/>
+//             Button floating icon='publish' className='green'/>
+//             <Button floating icon='attach_file' className='blue'/>
+//         </Button>
+//         );
+// }
