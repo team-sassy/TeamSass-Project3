783e4d38427ee6bb9784aae2ddb9737d1c59eb97
Lucent <lquint85@gmail.com>
2019-02-18 10:38:39 -0600

    styling

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -7,21 +7,26 @@ function Jumbotron() {
   return (
     <>
    
-      <div id="jumbotron" className="container text-center" width="1200">
+      <div id="jumbotron" className="container">
           <div className="Welcome text-center"> 
             <h1> Welcome to O.T.F</h1>
             <p> Begin your planning by today! </p>
-      </div>
-     
-      <div className="tabsContainer">
+          </div>
+
+
+          <div className="tabsContainer">
               <Button href="#" className="tabsButton" waves='light'>Flight<Icon left>flight</Icon></Button>
               <Button href="#" className="tabsButton" waves='light'>Dining<Icon left>restaurant</Icon></Button>
               <Button href="#" className="tabsButton" waves='light'>Activities<Icon left>golf_course</Icon></Button>
               <Button href="#" className="tabsButton" waves='light'>Itinerary<Icon left>calendar_today</Icon></Button>
-       </div>
-        
-      </div>
+           </div>
+        </div>
+
+
+
+
           <div id="jumbotron2"className="jumbotron text-center">
+              
               <div id="card1"className="row">
                 <div id="card"className="col s12 m6">
                   <div className="card blue-grey darken-1">
@@ -31,12 +36,13 @@ function Jumbotron() {
                       I am convenient because I require little markup to use effectively.</p>
                     </div>
                     <div className="card-action">
-                      <a href="#">This is a link</a>
-                      <a href="#">This is a link</a>
+                      <a href="Home">This is a link</a>
+                      <a href="Home">This is a link</a>
                     </div>
                   </div>
                 </div>
               </div>
+              
               <div id="card2" className="row">
                 <div id="card"className="col s12 m6">
                   <div className="card blue-grey darken-1">
@@ -46,15 +52,14 @@ function Jumbotron() {
                       I am convenient because I require little markup to use effectively.</p>
                     </div>
                     <div className="card-action">
-                      <a href="#">Maybe if You Click</a>
-                      <a href="#">Something Happens</a>
+                      <a href="Home">Maybe if You Click</a>
+                      <a href="HOme">Something Happens</a>
                     </div>
                   </div>
                 </div>
             </div>
         </div>
-
-          
+      
       </>
 
    );
