ac90060085fff42fc817e19e5f3cc202d7e1f4ec
kathleen.e.burns@gmail.com <kathleen.e.burns@gmail.com>
2019-02-19 20:01:41 -0600

    updated agenda

--- client/src/components/Itinerary/index.js
+++ client/src/components/Itinerary/index.js
@@ -1,39 +1,104 @@
-import React from 'react';
-import Dayz from 'dayz';
-// could also import the sass if you have a loader at dayz/dayz.scss
-import 'dayz/dist/styles.css';
-import moment from 'moment';
- 
-// would come from a network request in a "real" app
-const EVENTS = new Dayz.EventsCollection([
-    { content: 'A short event',
-      range: moment.range( date.clone(),
-                           date.clone().add(1, 'day') ) },
-    { content: 'Two Hours ~ 8-10',
-      range: moment.range( date.clone().hour(8),
-                           date.clone().hour(10) ) },
-    { content: "A Longer Event",
-      range: moment.range( date.clone().subtract(2,'days'),
-                           date.clone().add(8,'days') ) }
-]);
- 
-class MyComponent extends React.Component {
- 
-    render() {
-        return <Dayz
-                   display='week'
-                   date={this.props.date}
-                   events={EVENTS}
-               />
-    }
- 
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+import { Agenda } from 'react-native-calendars';
+
+export default class AgendaScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: {}
+    };
+  }
+
+  render() {
+    return (
+      <Agenda
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems.bind(this)}
+        selected={'2019-02-01'}
+        renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
+        // markingType={'period'}
+        // markedDates={{
+        //    '2017-05-08': {textColor: '#666'},
+        //    '2017-05-09': {textColor: '#666'},
+        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
+        //    '2017-05-21': {startingDay: true, color: 'blue'},
+        //    '2017-05-22': {endingDay: true, color: 'gray'},
+        //    '2017-05-24': {startingDay: true, color: 'gray'},
+        //    '2017-05-25': {color: 'gray'},
+        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
+         // monthFormat={'yyyy'}
+         // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+      />
+    );
+  }
+
+  loadItems(day) {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 5);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: 'Item for ' + strTime,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      //console.log(this.state.items);
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+    // console.log(`Load Items for ${day.year}-${day.month}`);
+  }
+
+  renderItem(item) {
+    return (
+      <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
+    );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  }
 }
 
-// const Itinerary = props => {
-//     return (
-//           <h1>Hello.  This is the itinerary page</h1>   
-
-//     )
-//     }
-
-// export default Itinerary
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex:1,
+    paddingTop: 30
+  }
+});
