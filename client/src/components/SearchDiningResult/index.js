c66c18a4d8201b1166143ae4a4c106253bbc8036
duncandam <duncandam86@gmail.com>
2019-02-20 13:56:23 -0600

    update style and search results

--- client/src/components/SearchDiningResult/index.js
+++ client/src/components/SearchDiningResult/index.js
@@ -1,51 +1,30 @@
 import React from "react";
 import "./style.css";
-import {Row, Col} from "../Grid"
+import { Col, Row, Card, CardTitle, Button } from 'react-materialize'
 
 const SearchDiningResult = props => {
     return (props.restaurants.length === 0) ? (
-        <div className="card"></div>
+        <></>
     ) : (
-            <div className="card">
-                <div className="card-body player">
-                    <div className="article">
-                        <h3>Search Results</h3>
-                        {props.restaurants.map(restaurant => {
-                            return (
-                                <li className="search-list list-group-item" key={restaurant.key}>
-                                    <Row className="SearchResult row" id={restaurant.name+ "Card"}>
-                                        <Col size="3" className="ResImage">
-                                            <img src={restaurant.image} alt={restaurant.name} />
-                                        </Col>
-            
-                                        {/* col-9 show information of the book */}
-                                        <Col size="9" className="ResInfo">
-                                            <Row>
-                                                <h4 className="restaurant_name">Name: {restaurant.name}</h4>
-                                            </Row>
-                                            <Row>
-                                                <h5 className="restaurant_location">Location: {restaurant.location}</h5>
-                                            </Row>
-                                            <Row>
-                                                <p className="restaurant_rating">Rating: {restaurant.rating}</p>
-                                            </Row>
-                                            <Row>
-                                                <a className="restaurant_url" href={restaurant.link} target="_blank">Link: {restaurant.name}</a>
-                                            </Row>
-                                        </Col>
-                                    </Row>
-                                    <br></br>
-                                    <Row className="buttonDiv ">
-                                        <button className="saveBook btn btn-primary" id={restaurant.id} onClick={(event) => props.handleSavedButton(event)}>
-                                            Save Place
-                                        </button>
-                                    </Row>
-                                </li>
-                            );
-                        })}
-                    </div>
-                </div>
-            </div>
+            <>
+                {props.restaurants.map(restaurant => {
+                    return (
+                        <Card className ="place-card"
+                            header={< CardTitle reveal image={restaurant.image} waves='light' />}
+                            title={restaurant.name}
+                            reveal={<>
+                                <p>Name: {restaurant.name}</p>
+                                <p>Location: {restaurant.location}</p>
+                                <p>Rating: {restaurant.rating}</p>
+                                <Button waves='light' className="saveBook btn btn-primary" id={restaurant.id} onClick={(event) => props.handleSavedButton(event)}>Save</Button>
+                            </>}>
+                            <p><a href={restaurant.link}>{restaurant.name}</a></p>
+                            <br></br>
+                        </Card >
+                        
+                    )
+                })}
+            </>
         )
 }
 export default SearchDiningResult
