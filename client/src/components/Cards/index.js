0e53425b8b8a09c1ef4424e0625e7abea0f4468d
Lucent <lquint85@gmail.com>
2019-02-20 21:47:33 -0600

    added carousel images to cards and sticky nav styling

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -3,30 +3,26 @@ import React from "react";
 // import Beach from "../../images/"
 import {Card,CardTitle} from "react-materialize";
 
+import MadridPic from '../../images/madrid.jpg'
+import JapanPic from '../../images/japan.jpg'
+
 function HomeCard1 () {
     return (
         <>
         <div className="card-container" > 
-            <Card className='small'
-                header={<CardTitle image='./images/chicago.jpg'>Chicago</CardTitle>}
-                actions={[<a href=''>Chicago</a>]}>
-                I am a very simple card. I am good at containing small bits of information. 
-               
+            
+            <Card  id="card2" className='small'
+                header={<CardTitle image={MadridPic}>Madrid</CardTitle>}
+                actions={[<a href=''>Madrid</a>]}>
+                The images can be changed to better serve the functionaily. 
+             
             </Card>
             <Card  id="card2" className='small'
-                header={<CardTitle image='client/src/images/japan.jpg'>Madrid</CardTitle>}
-                actions={[<a href='#'>Madrid</a>]}>
+                header={<CardTitle image={JapanPic}>Japan</CardTitle>}
+                actions={[<a href=''>Madrid</a>]}>
                 I am a very simple card. I am good at containing small bits of information. 
              
             </Card>
-            <Card id="card3"className='small'
-                header={<CardTitle image='img/sample-1.jpg'>Japan</CardTitle>}
-                actions={[<a href='#'>Japan</a>]}>
-                I am a very simple card. I am good at containing small bits of information. 
-             
-            </Card>
-
-      
         </div>
       
     
