2526d264001cd6f92a968efbd0839da962cfeaee
Lucent <lquint85@gmail.com>
2019-02-22 14:52:51 -0600

    added functionality to cards

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -2,9 +2,10 @@ import React from "react";
 // import "./style.css";
 // import Beach from "../../images/"
 import {Card,CardTitle} from "react-materialize";
+import HotelPic1 from '../../images/';
+import FlightPic1 from '../../images/';
+import DiningPic1 from '../../images/';
 
-import MadridPic from '../../images/madrid.jpg'
-import JapanPic from '../../images/japan.jpg'
 
 function HomeCard1 () {
     return (
@@ -12,13 +13,19 @@ function HomeCard1 () {
         <div className="card-container" > 
             
             <Card  id="card2" className='small'
-                header={<CardTitle image={MadridPic}>Madrid</CardTitle>}
+                header={<CardTitle image={FlightPic1}>Search Flights</CardTitle>}
                 actions={[<a href=''>Madrid</a>]}>
                 The images can be changed to better serve the functionaily. 
              
             </Card>
             <Card  id="card2" className='small'
-                header={<CardTitle image={JapanPic}>Japan</CardTitle>}
+                header={<CardTitle image={HotelPic1}>Search Hotels</CardTitle>}
+                actions={[<a href=''>Madrid</a>]}>
+                I am a very simple card. I am good at containing small bits of information. 
+             
+            </Card>
+            <Card  id="card2" className='small'
+                header={<CardTitle image={DiningPic1}>Search Dining</CardTitle>}
                 actions={[<a href=''>Madrid</a>]}>
                 I am a very simple card. I am good at containing small bits of information. 
              
