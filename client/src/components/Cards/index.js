d8e5011ca3c54546fd1b1c28becf25ec215a8088
Lucent <lquint85@gmail.com>
2019-02-23 10:03:14 -0600

    needs mod

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -2,34 +2,34 @@ import React from "react";
 // import "./style.css";
 // import Beach from "../../images/"
 import {Card,CardTitle} from "react-materialize";
-import HotelPic1 from '../../images/';
-import FlightPic1 from '../../images/';
-import DiningPic1 from '../../images/';
+// import HotelPic2 from '../../images/hotelpic2.jpg';
+import FlightPic1 from '../../images/flightpic1.jpg';
+// import DiningPic1 from '../../images/';
 
 
 function HomeCard1 () {
     return (
         <>
         <div className="card-container" > 
-            
+{/*             
             <Card  id="card2" className='small'
                 header={<CardTitle image={FlightPic1}>Search Flights</CardTitle>}
                 actions={[<a href=''>Madrid</a>]}>
                 The images can be changed to better serve the functionaily. 
              
-            </Card>
+            </Card> */}
             <Card  id="card2" className='small'
-                header={<CardTitle image={HotelPic1}>Search Hotels</CardTitle>}
-                actions={[<a href=''>Madrid</a>]}>
-                I am a very simple card. I am good at containing small bits of information. 
+                header={<CardTitle image={FlightPic1}>Flights</CardTitle>}
+                actions={[<a href='SearchFlightForm'>Search Flights</a>]}>
+                Ready to unwind? Click the search hotels link to find your perfect overnight stay. 
              
             </Card>
-            <Card  id="card2" className='small'
+            {/* <Card  id="card2" className='small'
                 header={<CardTitle image={DiningPic1}>Search Dining</CardTitle>}
                 actions={[<a href=''>Madrid</a>]}>
                 I am a very simple card. I am good at containing small bits of information. 
              
-            </Card>
+            </Card> */}
         </div>
       
     
